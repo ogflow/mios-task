eb9b9461db3b651a4d37fe443cc6e22f8b1001f6
Sergey Smirnov <smirnov.sege@gmail.com>
2019-06-06 09:47:38 +0300

    Updates

---
+++ precache-manifest.9728f6f0ab3f363fca8d51c8fac728ab.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243f595811c1d5038343c3b5e4ecd387",
+    "url": "/mios-task/index.html"
+  },
+  {
+    "revision": "c7799bc077b5a817e447",
+    "url": "/mios-task/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "068f64c9ea38c6fd9fd3",
+    "url": "/mios-task/static/css/main.855420ee.chunk.css"
+  },
+  {
+    "revision": "c7799bc077b5a817e447",
+    "url": "/mios-task/static/js/2.427ca6de.chunk.js"
+  },
+  {
+    "revision": "068f64c9ea38c6fd9fd3",
+    "url": "/mios-task/static/js/main.7f529607.chunk.js"
+  },
+  {
+    "revision": "eae0defc0675690433fe",
+    "url": "/mios-task/static/js/runtime~main.73132ee7.js"
+  },
+  {
+    "revision": "495e52cd235bd0f79d73d5af07fb09af",
+    "url": "/mios-task/static/media/Finland.495e52cd.svg"
+  },
+  {
+    "revision": "cd51a45b64eaaa2efd5362d9956e0c81",
+    "url": "/mios-task/static/media/VK.cd51a45b.svg"
+  },
+  {
+    "revision": "bbb22c5d6749edc9af12d1948fc9afd9",
+    "url": "/mios-task/static/media/arrow-down.bbb22c5d.svg"
+  },
+  {
+    "revision": "11257bcb19b342b7c5e9ef1ca2b9379a",
+    "url": "/mios-task/static/media/cart.11257bcb.svg"
+  },
+  {
+    "revision": "e5f820522b2d69a67753e12a85e0c299",
+    "url": "/mios-task/static/media/emoji-wave-hand.e5f82052.png"
+  },
+  {
+    "revision": "fb748b5f4b6b1a314b6aa0a2a852ff67",
+    "url": "/mios-task/static/media/facebook.fb748b5f.svg"
+  },
+  {
+    "revision": "84ff9b47c2ea941b5f9ff3b337d62a8b",
+    "url": "/mios-task/static/media/instagram.84ff9b47.svg"
+  },
+  {
+    "revision": "ea530705cc540a13e6de584860284bb9",
+    "url": "/mios-task/static/media/left-arrow.ea530705.svg"
+  },
+  {
+    "revision": "82fd9c30e2e700ca08c4ef668e55f4ea",
+    "url": "/mios-task/static/media/logo.82fd9c30.svg"
+  },
+  {
+    "revision": "c44537fae7e7e372d25a6470391fee4b",
+    "url": "/mios-task/static/media/long-arrow-down.c44537fa.svg"
+  },
+  {
+    "revision": "9e2096dd46abc47cf13cbc98523c1c7e",
+    "url": "/mios-task/static/media/menu.9e2096dd.svg"
+  },
+  {
+    "revision": "a1cf8f12b2b6636a6c0077a7ca8be267",
+    "url": "/mios-task/static/media/picture-1.a1cf8f12.jpg"
+  },
+  {
+    "revision": "608d00c9a51bd5a8a8ae0d08b871533d",
+    "url": "/mios-task/static/media/picture-2.608d00c9.jpg"
+  },
+  {
+    "revision": "98f38c3539c6199aa853d6b9e1b60bf5",
+    "url": "/mios-task/static/media/pinterest.98f38c35.svg"
+  },
+  {
+    "revision": "3ab4b4830730338ad40a9b5579de062a",
+    "url": "/mios-task/static/media/pircture-3.3ab4b483.jpg"
+  },
+  {
+    "revision": "14e7a163e5a39e31b198565b4fa6fe16",
+    "url": "/mios-task/static/media/right-arrow.14e7a163.svg"
+  },
+  {
+    "revision": "4bdf0f0834ce441247c673e7347ca9e3",
+    "url": "/mios-task/static/media/search.4bdf0f08.svg"
+  },
+  {
+    "revision": "8664f3cfd913d68414cfa04d0d3823ac",
+    "url": "/mios-task/static/media/user.8664f3cf.svg"
+  }
+]);
